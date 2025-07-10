b957af44b0404401667e346fd09756d00f750083
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:36 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Haunted_0.1" `
+  -ModName "Haunted 0.1" `
+  -ModFolder "Haunted" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/haunted/Haunted-0.1-Original.zip" `
+  -ModBaseFilesUrlHash "4accf807ef40ac659b45b1bddb5eac573bd6a06b8a0067cabf5063c27b956cfe" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
